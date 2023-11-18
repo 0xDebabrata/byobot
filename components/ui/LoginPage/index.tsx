2336ab8e074262d8c3311e80b473663553f40b62
Debabrata Mondal <debabrata.js@protonmail.com>
2023-11-18 10:05:06 +0530

    fix getURL

--- components/ui/LoginPage/index.tsx
+++ components/ui/LoginPage/index.tsx
@@ -13,7 +13,7 @@ const getURL = () => {
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  // url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
   return url;
 };
 
